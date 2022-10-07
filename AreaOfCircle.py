ffca9c372f5b9c2e88d081cf76d951118f48e566
BlendInto <83698951+BlendInto@users.noreply.github.com>
2022-10-07 22:02:44 +0530

    Add files via upload

---
+++ AreaOfCircle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+r = float(input("Enter the radius of your circle: "))
+print("\n")
+pi = 3.1416
+area = (r**2)*pi
+print("Area of the circle: ")
+print(area)
---
+++ Extension_check.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 20:51:17 2022
+
+@author: saatv
+"""
+
+str1 = input("Enter File name:" )
+if (str1.endswith(".py") == True):
+    print("The extension of the file is python")
+else:
+    print("Invalid file extension")
+
+    
